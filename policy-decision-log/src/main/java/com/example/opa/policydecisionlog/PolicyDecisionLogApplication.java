059b1d420bd9548444224f05b6f5a0e2be4cd509
이정진 <jeongjinlee@nhn.com>
2025-12-18 11:31:52 +0900

    feat: policy-decision-log Spring Boot 프로젝트 초기 설정

---
+++ policy-decision-log/src/main/java/com/example/opa/policydecisionlog/PolicyDecisionLogApplication.java
@@ -0,0 +1,13 @@
+package com.example.opa.policydecisionlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolicyDecisionLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolicyDecisionLogApplication.class, args);
+	}
+
+}
---
+++ policy-decision-log/src/test/java/com/example/opa/policydecisionlog/PolicyDecisionLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.opa.policydecisionlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolicyDecisionLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
